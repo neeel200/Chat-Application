240ef54fd7b9e9ec8e641e1bbcdbb77d2b6f1fb1
neeel101 <neeleshvinchoorkar@gmail.com>
2024-05-05 09:56:11 +0530

    backend completed

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+        dbName:process.env.DB_Name,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
